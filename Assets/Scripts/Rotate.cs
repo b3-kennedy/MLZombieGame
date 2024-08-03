962b40be24a40e32151840f5fdbe48581eb5e8c8
b3-kennedy <benjamin3.kennedy@live.uwe.ac.uk>
2024-08-03 14:04:42 +0100

    added key and padlock models

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -18,6 +18,6 @@ public class Rotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(rotateVec.x * rotateSpeed * Time.deltaTime, rotateVec.y * rotateSpeed * Time.deltaTime, rotateVec.z * rotateSpeed * Time.deltaTime, Space.Self);
+        transform.Rotate(rotateVec.x * rotateSpeed * Time.deltaTime, rotateVec.y * rotateSpeed * Time.deltaTime, rotateVec.z * rotateSpeed * Time.deltaTime, Space.Self);
     }
 }
